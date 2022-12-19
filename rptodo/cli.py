30d2a0702c67de94aad53c17237e9322e5f35ad4
hustchihieu143 <chihieusky@gmail.com>
2022-12-19 17:34:34 +0700

    doing cli

--- rptodo/cli.py
+++ rptodo/cli.py
@@ -1,13 +1,79 @@
 """This module provides the RP To-Do CLI."""
 # rptodo/cli.py
 
+from pathlib import Path
+from typing import List, Optional
 from typing import Optional
 
 import typer
 
-from rptodo import __app_name__, __version__
+from rptodo import (
+    ERRORS, __app_name__, __version__, config, database, rptodo
+)
 
 app = typer.Typer()
+
+def get_todoer() -> rptodo.Todoer:
+    if config.CONFIG_FILE_PATH.exists():
+        db_path = database.get_database_path(config.CONFIG_FILE_PATH)
+    else:
+        typer.secho(
+            'Config file not found. Please, run "rptodo init"',
+            fg=typer.colors.RED,
+        )
+        raise typer.Exit(1)
+    if db_path.exists():
+        return rptodo.Todoer(db_path)
+    else:
+        typer.secho(
+            'Database not found. Please, run "rptodo init"',
+            fg=typer.colors.RED,
+        )
+        raise typer.Exit(1)
+
+@app.command()
+def init(
+    db_path: str = typer.Option(
+        str(database.DEFAULT_DB_FILE_PATH),
+        "--db-path",
+        "-db",
+        prompt="to-do database location?",
+    ),
+) -> None:
+    """Initialize the to-do database."""
+    app_init_error = config.init_app(db_path)
+    if app_init_error:
+        typer.secho(
+            f'Creating config file failed with "{ERRORS[app_init_error]}"',
+            fg=typer.colors.RED,
+        )
+        raise typer.Exit(1)
+    db_init_error = database.init_database(Path(db_path))
+    if db_init_error:
+        typer.secho(
+            f'Creating database failed with "{ERRORS[db_init_error]}"',
+            fg=typer.colors.RED,
+        )
+        raise typer.Exit(1)
+    else:
+        typer.secho(f"The to-do database is {db_path}", fg=typer.colors.GREEN)
+
+
+@app.command()
+def test(
+    db_path: str = typer.Option(
+        str(database.DEFAULT_DB_FILE_PATH),
+        "--test",
+        "-test",
+    ),
+) -> None:
+    """Initialize the to-do database."""
+    typer.secho(
+            f'Creating database failed with',
+            fg=typer.colors.RED,
+        )
+    typer.secho(f"The to-do database is {db_path}", fg=typer.colors.GREEN)
+
 
 def _version_callback(value: bool) -> None:
     if value:
